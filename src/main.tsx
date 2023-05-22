83ec6757492a879f68b5775e2db6dbaa2437a164
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-23 01:21:52 +0200

    utilisation login context provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from "./App.tsx";
 import "./main.css";
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import LoginContextProvider from "./components/LoginContextProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <BrowserRouter>
-        <App />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <LoginContextProvider>
+          <App />
+        </LoginContextProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
