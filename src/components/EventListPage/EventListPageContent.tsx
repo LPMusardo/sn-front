a1402da7136206cb292e49c384a24a8083dfede5
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-29 01:35:09 +0200

    Mapping API et Page de recherche des events

--- src/components/EventListPage/EventListPageContent.tsx
+++ src/components/EventListPage/EventListPageContent.tsx
@@ -3,31 +3,53 @@ import Footer from "../shared/Footer";
 import NavBar from "../shared/NavBar/NavBar";
 import FiltersToggle from "./FiltersToggle";
 import ResultList from "./ResultList";
-import { SearchContext } from "./SearchContextProvider";
 import { useContext, useEffect } from "react";
 import SearchInputContext from "../shared/NavBar/SearchInputContext";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import SearchInput2 from "./SearchInput2";
+
+// event_name: z.string(),
+// description: z.string(),
+// category: z.string(),
+// date_min: z.coerce.date(), // max>min
+// date_max: z.coerce.date(),
+// main_category: z.enum(["1", "2", "3", ""]),
+// participants_min: z.string().regex(/^[0-9]*$/), //max>min
+// participants_max: z.string().regex(/^[0-9]*$/),
+// street: z.string(),
+// city: z.string(),
+// country: z.string(),
+// zip: z.string(),
 
 const EventListPage = () => {
-  const [search, setSearch] = useContext(SearchContext);
+  // const [search, setSearch] = useContext(SearchContext);
 
-  const location = useLocation();
-  const eventName = new URLSearchParams(location.search).get("eventName");
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (eventName == undefined) {
-      setSearch("vide");
-      return;
-    }
-    setSearch(eventName);
-  }, []);
+    // if (eventName == undefined) {
+    //   setSearch("vide");
+    //   return;
+    // }
+    // setSearch(eventName);
+    console.log("coucou depuis ListPage");
+  });
 
   return (
     <>
       <NavBar
-        onSearch={() => {}}
-        searchInput={<SearchInputContext onSearch={() => {}} />}
+        // onSearch={() => {}}
+        searchInput={<SearchInput2 />}
       />
+
+      {/* <div>{ search}</div> */}
+      {/* <div>{ JSON.stringify(getQueryParams(), null, 2).toString()}</div> */}
+      {/* <Button
+        colorScheme="teal"
+        onClick={() => navigate("/search?event_name=babar")}
+      >
+        Test
+      </Button> */}
 
       <Box margin={5} minH="60vh">
         <FiltersToggle />
--- src/components/EventListPage/ResultList.tsx
+++ src/components/EventListPage/ResultList.tsx
@@ -15,32 +15,43 @@ import {
   Center,
   Show,
   VStack,
+  Spinner,
 } from "@chakra-ui/react";
 import LeftPanel from "../PublicUserPage/LeftPanel";
 import RightPanel from "../PublicUserPage/RightPanel";
 import EventListingCard from "./EventListingCard";
+import { useFetchSearch } from "./FetchSearchContextProvider";
 
 const ResultList = () => {
+  const [events, error, isLoading, fetchEvents] = useFetchSearch();
+
   return (
-    <Grid
-      templateColumns={{ sm: "1fr", md: "1fr 1fr" }}
-      gap={10}
-      mx={{ sm: "5", lg: "100" }}
-      my="35"
-    >
-      <GridItem>
-        <VStack spacing="5">
-          <EventListingCard />
-          <EventListingCard />
-        </VStack>
-      </GridItem>
-      <GridItem>
-        <VStack spacing="5">
-          <EventListingCard />
-          <EventListingCard />
-        </VStack>
-      </GridItem>
-    </Grid>
+    <>
+      {error && <Text color="red">{error}</Text>}
+      <Grid
+        templateColumns={{ sm: "1fr", md: "1fr 1fr" }}
+        gap={10}
+        mx={{ sm: "5", lg: "100" }}
+        my="35"
+        >
+        <GridItem>
+        {isLoading && <Spinner />}
+          <VStack spacing="5">
+            {events.slice(0, Math.ceil(events.length / 2)).map((event) => (
+              <EventListingCard key={event.id} event={event} />
+              ))}
+          </VStack>
+        </GridItem>
+        <GridItem>
+        {isLoading && <Spinner />}
+          <VStack spacing="5">
+            {events.slice(Math.ceil(events.length / 2)).map((event) => (
+              <EventListingCard key={event.id} event={event} />
+              ))}
+          </VStack>
+        </GridItem>
+      </Grid>
+    </>
   );
 };
 
