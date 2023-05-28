85fcaba4067f33771e9a3c600f1db7d281b5cf32
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-29 01:31:47 +0200

    refonte de la gestion des routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,15 @@ import EventPage from "./components/EventPage/EventPage";
 import EventListPage from "./components/EventListPage/EventListPage";
 import Register from "./components/AuthPage/Register";
 import Login from "./components/AuthPage/Login";
-import LoginContextProvider from "./components/LoginContextProvider";
-
+import PanelApplications from "./components/PrivateUserPage/Panels/PanelApplications/PanelApplications";
+import TestPage from "./components/TestPage/TestPage";
+import TestNestedComposant from "./components/TestPage/TestNestedComposant";
+import PanelInformations from "./components/PrivateUserPage/Panels/PanelInformations";
+import PanelGivenNotes from "./components/PrivateUserPage/Panels/PanelGivenNotes";
+import PanelEvents from "./components/PrivateUserPage/Panels/PanelEvents/PanelEvents";
+import PanelReceivedNotes from "./components/PrivateUserPage/Panels/PanelReceivedNotes";
+import TestPageRouting from "./components/TestPage/TestPageRouting";
+import PanelContainer from "./components/PrivateUserPage/PanelContainer";
 
 function App() {
   // const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
@@ -15,14 +22,17 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" Component={MainPage} />
-        <Route path="/login" Component={Login} />
-        <Route path="/signup" Component={Register} />
-        <Route path="/users/:id" Component={PublicUserPage} />
-        <Route path="/profile" Component={PrivateUserPage} />
-        <Route path="/profile/:sectionId" Component={PrivateUserPage} />
-        <Route path="/events/:id" Component={EventPage} />
-        <Route path="/search" Component={EventListPage} />
+        <Route index Component={MainPage} />
+        <Route path="login" Component={Login} />
+        <Route path="signup" Component={Register} />
+        <Route path="users/:id" Component={PublicUserPage} />
+        <Route path="events/:id" Component={EventPage} />
+        <Route path="search" Component={EventListPage} />
+
+        <Route path="/profile/*" element={<PrivateUserPage />}/>
+
+        <Route path="/test/*" element={<TestPageRouting />}/>
+        <Route path="*" Component={MainPage} />
       </Routes>
     </>
   );
