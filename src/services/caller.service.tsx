e51ba26dd684522819744465dd40b48b8ca4386d
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-29 01:31:14 +0200

    navigate vers /login si erreur 401

--- src/services/caller.service.tsx
+++ src/services/caller.service.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { tokenService } from "./token.service";
-
+import { tokenService } from "../services/token.service";
+import { useNavigate } from "react-router-dom";
+import { ReactElement } from "react";
 const Axios = axios.create({
   baseURL: "http://localhost:3000",
 });
@@ -9,8 +10,33 @@ Axios.interceptors.request.use((request) => {
   if (tokenService.getToken) {
     request.headers.Authorization = "Bearer " + tokenService.getToken();
   }
-
   return request;
 });
+
+
+Axios.interceptors.response.use(
+  (response) => { 
+    return response
+  },
+  (error) => {
+    console.log("AXIOS ERROR INTERCEPT---->", error);
+    // const navigate = useNavigate();
+    // navigate('/');
+    if(!error || !error.response || ! error.response.status) return;
+    const status = error.response.status;
+    if (status === 401) {
+      tokenService.removeToken();
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+
+// const API = ({ children }:{children:ReactElement}) => {
+//   return <>{children}</>;
+// };
+// export {API};
+
 
 export default Axios;
