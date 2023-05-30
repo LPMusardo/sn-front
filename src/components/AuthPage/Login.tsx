b1d15ad69552fd506318e64c66b9555602ce8407
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-30 16:01:50 +0200

    password size required

--- src/components/AuthPage/Login.tsx
+++ src/components/AuthPage/Login.tsx
@@ -26,7 +26,7 @@ function Login() {
     email: z.string().min(1, { message: "Email is required" }),
     password: z
       .string()
-      .min(6, { message: "Password must be atleast 6 characters" }),
+      .min(4, { message: "Password must be at least 4 characters" }),
   });
   
   type ValidationSchema = z.infer<typeof schema>;
