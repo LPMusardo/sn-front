5f0961aa677b260cf34403b2ed3c66aec307b978
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-31 18:34:01 +0200

    composant Filling utiluse Hook

---
+++ src/components/shared/Filling.tsx
@@ -0,0 +1,22 @@
+import { HStack, Text } from "@chakra-ui/react"
+import { RiGroupLine } from "react-icons/ri"
+import useFilling, { EventFilling } from "../../Hooks/useFilling" 
+import { CandidateEvent } from "../PrivateUserPage/MyApplicationsContextProvider"
+
+interface Props{
+    event: CandidateEvent
+}
+
+const Filling = ({event}:Props) => {
+
+    const [eventFilling] = useFilling(event.id)
+
+    return (
+        <HStack>
+            <RiGroupLine />
+            <Text>{`${eventFilling.nb_participants} / ${event.participants_number}`}</Text>
+        </HStack>
+    )
+}
+
+export default Filling
